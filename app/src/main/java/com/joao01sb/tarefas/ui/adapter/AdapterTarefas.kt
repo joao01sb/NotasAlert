40c5ac62bfe83342df245ae1e424cb7a0c7f85a0
joao01sb <bonfimnew@gmail.com>
2022-12-25 17:49:45 -0300

    adicionando funcoes

---
+++ app/src/main/java/com/joao01sb/tarefas/ui/adapter/AdapterTarefas.kt
@@ -0,0 +1,28 @@
+package com.joao01sb.tarefas.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.joao01sb.tarefas.databinding.TarefaItemBinding
+import com.joao01sb.tarefas.model.Tarefa
+
+class AdapterTarefas(
+    private val tarefas: List<Tarefa> = emptyList(),
+    var onItemClickListener: (tarefa: Tarefa) -> Unit = {}
+) : RecyclerView.Adapter<AdapterTarefas.ViewHolder>() {
+
+    inner class ViewHolder(
+        private val binding: TarefaItemBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun vincularPersonagemComDados(tarefa: Tarefa) {
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+        ViewHolder(TarefaItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = tarefas.get(position).let { holder.vincularPersonagemComDados(it) }
+
+    override fun getItemCount(): Int = tarefas.size
+}
---
+++ app/src/main/java/com/joao01sb/tarefas/ui/fragment/FragmentListaTarefas.kt
@@ -0,0 +1,25 @@
+package com.joao01sb.tarefas.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.joao01sb.tarefas.R
+
+class FragmentListaTarefas : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_lista_tarefas, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
