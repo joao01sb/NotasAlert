9b85bd5bbd5c7aa95926abebf576a29c550b6782
joao01sb <bonfimnew@gmail.com>
2023-01-24 23:18:22 -0300

    adicionando intent ao clicar na notificacao

--- app/src/main/java/com/joao01sb/tarefas/notification/Notification.kt
+++ app/src/main/java/com/joao01sb/tarefas/notification/Notification.kt
@@ -1,11 +1,14 @@
 package com.joao01sb.tarefas.notification
 
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import androidx.core.app.NotificationCompat
 import com.joao01sb.tarefas.R
+import com.joao01sb.tarefas.ui.MainActivity
 
 const val notificationID = 1
 const val channelID = "channel1"
@@ -16,10 +19,19 @@ class Notification : BroadcastReceiver()
 {
     override fun onReceive(context: Context, intent: Intent)
     {
+
+        val resultIntent = Intent(context, MainActivity::class.java)
+        val resultPendingIntent: PendingIntent? = TaskStackBuilder.create(context).run {
+            addNextIntentWithParentStack(resultIntent)
+            getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+        }
+
         val notification = NotificationCompat.Builder(context, channelID)
             .setSmallIcon(R.drawable.ic_launcher_foreground)
             .setContentTitle(intent.getStringExtra(titleExtra))
             .setContentText(intent.getStringExtra(messageExtra))
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentIntent(resultPendingIntent)
             .build()
 
         val  manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
